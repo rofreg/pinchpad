310c7c6fc7bd8ab2e57661869092110a7fbca784
Ryan Laughlin <rofreg@gmail.com>
2017-05-30 23:06:54 -0400

    Initial JotUI setup? Not working yet though

---
+++ PinchPad/PinchPad-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  PinchPad-Bridging-Header.h
+//  PinchPad
+//
+//  Created by Ryan Laughlin on 5/30/17.
+//  Copyright © 2017 Ryan Laughlin. All rights reserved.
+//
+
+#ifndef PinchPad_Bridging_Header_h
+#define PinchPad_Bridging_Header_h
+
+#import <JotUI/JotUI.h>
+
+#endif /* PinchPad_Bridging_Header_h */
--- PinchPad/ViewController.swift
+++ PinchPad/ViewController.swift
@@ -12,7 +12,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Set up a view for drawing
+        let jotView = JotView(frame: CGRect(x: 50, y: 50, width: 200, height: 200))
+        jotView.delegate = self
+        jotView.backgroundColor = .red
+        view.insertSubview(jotView, at: 0)
+
+        let jotViewStateProxy = JotViewStateProxy()
+        jotViewStateProxy.delegate = self
+        jotViewStateProxy.loadJotStateAsynchronously(false,
+                                                     with: jotView.bounds.size,
+                                                     andScale: UIScreen.main.scale,
+                                                     andContext: jotView.context,
+                                                     andBufferManager: JotBufferManager.sharedInstance())
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,4 +33,87 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+}
+
+extension ViewController: JotViewDelegate {
+
+    func textureForStroke() -> JotBrushTexture! {
+        print("textureForStroke")
+        return JotDefaultBrushTexture.sharedInstance()
+    }
+
+    func stepWidthForStroke() -> CGFloat {
+        print("stepWidthForStroke")
+        return 2
+    }
+
+    func supportsRotation() -> Bool {
+        print("supportsRotation")
+        return false
+    }
+
+    func width(forCoalescedTouch coalescedTouch: UITouch!, from touch: UITouch!) -> CGFloat {
+        print("width")
+        return 1
+    }
+
+    func color(forCoalescedTouch coalescedTouch: UITouch!, from touch: UITouch!) -> UIColor! {
+        print("color")
+        return .black
+    }
+
+    func smoothness(forCoalescedTouch coalescedTouch: UITouch!, from touch: UITouch!) -> CGFloat {
+        print("smoothness")
+        return 0.75
+    }
+
+    func willAddElements(_ elements: [Any]!,
+                         to stroke: JotStroke!,
+                         fromPreviousElement previousElement: AbstractBezierPathElement!) -> [Any]! {
+        print("willAddElements")
+        return elements
+    }
+
+    func willBeginStroke(withCoalescedTouch coalescedTouch: UITouch!, from touch: UITouch!) -> Bool {
+        print("willBeginStroke")
+        return true
+    }
+
+    func willMoveStroke(withCoalescedTouch coalescedTouch: UITouch!, from touch: UITouch!) {
+        print("willMoveStroke")
+    }
+
+    func willEndStroke(withCoalescedTouch coalescedTouch: UITouch!, from touch: UITouch!, shortStrokeEnding: Bool) {
+        print("willEndStroke")
+    }
+
+    func didEndStroke(withCoalescedTouch coalescedTouch: UITouch!, from touch: UITouch!) {
+        print("didEndStroke")
+    }
+
+    func willCancel(_ stroke: JotStroke!, withCoalescedTouch coalescedTouch: UITouch!, from touch: UITouch!) {
+        print("willCancel")
+    }
+
+    func didCancel(_ stroke: JotStroke!, withCoalescedTouch coalescedTouch: UITouch!, from touch: UITouch!) {
+        print("didCancel")
+    }
+}
+
+extension ViewController: JotViewStateProxyDelegate {
+    func didLoadState(_ state: JotViewStateProxy!) {
+        print("didLoadState")
+    }
+
+    func didUnloadState(_ state: JotViewStateProxy!) {
+        print("didUnloadState")
+    }
+
+    var jotViewStatePlistPath: String! {
+        return "state.plist"
+    }
+
+    var jotViewStateInkPath: String! {
+        return "ink.png"
+    }
 }
