4470def2bac52de3ff02cacde26b9482789df4bd
Ryan Laughlin <rofreg@gmail.com>
2019-09-21 20:49:12 -0400

    Improve tap-to-dismiss behavior for image preview

--- PinchPad/ImagePreviewViewController.swift
+++ PinchPad/ImagePreviewViewController.swift
@@ -32,7 +32,7 @@ class ImagePreviewViewController: UIViewController {
         }
     }
 
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.dismiss(animated: true, completion: nil)
     }
 }
